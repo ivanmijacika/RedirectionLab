9385003f4ced55aea9ff948193c5a9eee9432541
ivanmijacika <imijacika20@stuy.edu>
2021-01-04 21:25:57 -0500

    spacing edits

--- MakeStars.java
+++ MakeStars.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class MakeStars{
-  public static void main( String[]args ){
-      Scanner n = new Scanner( System.in );
+  public static void main(String[]args){
+      Scanner n = new Scanner(System.in);
       while (n.hasNextLine()){
         String s = n.nextLine();
         Scanner n1 = new Scanner(s);
