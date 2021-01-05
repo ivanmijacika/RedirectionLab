9dfbf633cec853c6ec13624575e04d3d846a4caf
ivanmijacika <imijacika20@stuy.edu>
2021-01-04 19:37:28 -0500

    added a line to deal with  as imput

--- PigLatin.java
+++ PigLatin.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class PigLatin{
 
   public static String pigLatinSimple(String s){
+    if (s.equals("")) return s;
     s = s.toLowerCase();
     if (s.charAt(0)=='a' || s.charAt(0)=='e' || s.charAt(0)=='i' || s.charAt(0)=='o' || s.charAt(0)=='u') return (s+"hay");
     else if (s.length() > 1) return (s.substring(1) + s.charAt(0) + "ay");
